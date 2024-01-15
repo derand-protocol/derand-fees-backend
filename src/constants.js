ff83df92c879cc9648359f23da32faba0283f693
Shayan Shiravani <shayan5420@yahoo.com>
2024-01-15 21:52:23 +0330

    Add more chains

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,34 @@ export const EVM_NETWORKS = {
     graph_url: "https://api.thegraph.com/subgraphs/name/shayanshiravani/derand-mumbai",
     decimals: 18,
     coingecko_id: "matic-network"
+  },
+  97: {
+    rpc_url: "https://bsc-testnet.publicnode.com",
+    coordinator_address: "0xfb0522d3c1F3236560f79040f01A35af327098c9",
+    graph_url: "https://api.thegraph.com/subgraphs/name/shayanshiravani/derand-bsc-testnet",
+    decimals: 18,
+    coingecko_id: "binancecoin"
+  },
+  11155111: {
+    rpc_url: "https://rpc.ankr.com/eth_sepolia",
+    coordinator_address: "0x518D6d9e790457C9E822894D4AeBB757a170b443",
+    graph_url: "https://api.thegraph.com/subgraphs/name/shayanshiravani/derand-sepolia",
+    decimals: 18,
+    coingecko_id: "ethereum"
+  },
+  5: {
+    rpc_url: "https://rpc.ankr.com/eth_goerli",
+    coordinator_address: "0x52136E7D4714214f3fEF223648f984b08700da23",
+    graph_url: "https://api.thegraph.com/subgraphs/name/shayanshiravani/derand-goerli",
+    decimals: 18,
+    coingecko_id: "ethereum"
+  },
+  43113: {
+    rpc_url: "https://rpc.ankr.com/avalanche_fuji",
+    coordinator_address: "0x3D1CBd70145b4025579DBFbBe643517C299bD423",
+    graph_url: "https://api.thegraph.com/subgraphs/name/shayanshiravani/derand-avalanche-fuji",
+    decimals: 18,
+    coingecko_id: "avalanche-2"
   }
 }
 
