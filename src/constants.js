539fcd790fd7894d26978da1e3081a1dec85277e
Shayan Shiravani <shayan6959@gmail.com>
2024-05-09 21:44:02 +0330

    more chains

--- src/constants.js
+++ src/constants.js
@@ -19,7 +19,21 @@ export const EVM_NETWORKS = {
     graph_url: "https://api.studio.thegraph.com/query/71642/derand-optimism/version/latest",
     decimals: 18,
     coingecko_id: "ethereum"
-  }
+  },
+  8453: {
+    rpc_url: "https://rpc.ankr.com/base",
+    coordinator_address: "0x24092A71ae67b6773F6ecdc56a9E153e5C57D2E7",
+    graph_url: "https://api.studio.thegraph.com/query/71642/derand-base/version/latest",
+    decimals: 18,
+    coingecko_id: "ethereum"
+  },
+  534352: {
+    rpc_url: "https://rpc.scroll.io",
+    coordinator_address: "0x24092A71ae67b6773F6ecdc56a9E153e5C57D2E7",
+    graph_url: "https://api.studio.thegraph.com/query/71642/derand-scroll/version/latest",
+    decimals: 18,
+    coingecko_id: "ethereum"
+  },
 }
 
 export const FULFILL_LOG_ABI = [
